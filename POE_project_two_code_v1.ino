9a60ba2abf32291dcd6212ffcb020c692831b6a9
mm12028 <75343317+mm12028@users.noreply.github.com>
2021-09-25 17:46:58 -0400

    Add files via upload

--- POE_project_two_code_v1.ino
+++ POE_project_two_code_v1.ino
@@ -1,32 +1,47 @@
 
 #include <Servo.h>
+#include<SharpIR.h>
+#define IRPin A0
+#define model 20150;
 
 Servo One;  // create servo object to control a servo
-//Servo Two
+Servo Two;
+
 // twelve servo objects can be created on most boards
 
 int posone = 0;    // variable to store the servo position
-//int postwo = 0;    // variable to store the servo position
-int outputValue = 0;        // value output to the PWM (analog out)
+int postwo = 0;    // variable to store the servo position
+
+int AngleOne = 0; // Angle of sevro one
+int AngleTwo = 0; // Angle of servo two
+int sensorValue;
+
 
 void setup() {
   Serial.begin(9600); // initialize serial communications at 9600 bps:
   One.attach(9);  // attaches the servo on pin 9 to the servo object
- // Two.attach(10);  // attaches the servo on pin 10 to the servo object
+  Two.attach(10);  // attaches the servo on pin 10 to the servo object
+  
+  One.write(posone);
+  Two.write(postwo); 
+  delay(500);
 }
 
 void loop() {
-  for (posone = 0; posone <= 180; posone += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(posone);              // tell servo to go to position in variable 'pos'
-    outputValue = map(sensorValue, 0, 1023, 0, 255);  // map it to the range of the analog out:
-    analogWrite(analogOutPin, outputValue);// change the analog out value:
-    Serial.print("\t output = "); // print the results to the Serial Monitor:
-    Serial.println(outputValue);
+  for (posone = 0; posone <= 180; posone += 10) { // goes from 0 degrees to 180 degrees// in steps of 1 degree
+    One.write(posone);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
+    
+    for (postwo = 180; postwo >= 0; postwo -= 1) { // goes from 180 degrees to 0 degrees
+      Two.write(postwo);  // tell servo to go to position in variable 'pos'
+      sensorValue = analogRead(A0);
+      Serial.print("angle one = ");
+      Serial.print(posone);
+      Serial.print("\t angle two = ");
+      Serial.print(postwo);
+      Serial.print("\t Sensor Value = ");
+      Serial.println(sensorValue);
+      delay(15);          // waits 15ms for the servo to reach the position
+    }
   }
-//  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-}
+ }
